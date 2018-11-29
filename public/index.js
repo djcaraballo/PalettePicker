95d9cb5cdb577180aa084a66652a8dd4bb0c58ad
Dina <dcaraballo26@gmail.com>
2018-11-28 20:16:54 -0700

    Add lock and refresh buttons for colors and variable names to js file

--- public/index.js
+++ public/index.js
@@ -1,2 +1,23 @@
 //add option to drop down menu
-const 
+//inner html of section injected via js
+//change unlocked image to locked on click
+const firstColorSection = document.querySelector('.first-color');
+const secondColorSection = document.querySelector('.second-color');
+const thirdColorSection = document.querySelector('.third-color');
+const fourthColorSection = document.querySelector('.first-color');
+const fifthColorSection = document.querySelector('.first-color');
+const hex1 = document.querySelector('.hex1');
+const hex2 = document.querySelector('.hex2');
+const hex3 = document.querySelector('.hex3');
+const hex4 = document.querySelector('.hex4');
+const hex5 = document.querySelector('.hex5');
+const unlockedBtn1 = document.querySelector('.unlocked-btn1');
+const unlockedBtn2 = document.querySelector('.unlocked-btn2');
+const unlockedBtn3 = document.querySelector('.unlocked-btn3');
+const unlockedBtn4 = document.querySelector('.unlocked-btn4');
+const unlockedBtn5 = document.querySelector('.unlocked-btn5');
+const refreshBtn1 = document.querySelector('.refresh-btn1');
+const refreshBtn2 = document.querySelector('.refresh-btn2');
+const refreshBtn3 = document.querySelector('.refresh-btn3');
+const refreshBtn4 = document.querySelector('.refresh-btn4');
+const refreshBtn5 = document.querySelector('.refresh-btn5');
